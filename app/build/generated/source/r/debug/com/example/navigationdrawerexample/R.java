b74f30d62c93267815cea4e7cbe4ddf03373f10e
rajtrivedi88 <rajtrivedi88@gmail.com>
2016-05-28 11:07:39 +0530

    added changes 2

--- app/build/generated/source/r/debug/com/example/navigationdrawerexample/R.java
+++ app/build/generated/source/r/debug/com/example/navigationdrawerexample/R.java
@@ -18,14 +18,15 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int cabs=0x7f020000;
-        public static final int ic_action_copy=0x7f020001;
-        public static final int ic_action_refresh=0x7f020002;
-        public static final int ic_action_share=0x7f020003;
+        public static final int cab=0x7f020000;
+        public static final int flight=0x7f020001;
+        public static final int ic_action_copy=0x7f020002;
+        public static final int ic_action_refresh=0x7f020003;
         public static final int ic_drawer=0x7f020004;
         public static final int ic_launcher=0x7f020005;
         public static final int recharge=0x7f020006;
-        public static final int whatsapp=0x7f020007;
+        public static final int settings=0x7f020007;
+        public static final int whatsapp_new=0x7f020008;
     }
     public static final class id {
         public static final int action_settings=0x7f090005;
